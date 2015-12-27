ed39529a721bbb9e788ca9fd9fcfc9ebc1f525b1
lujun9972 <lujun9972@gmail.com>
2015-12-27 10:06:52 +0800

    translate doc-string to english

--- org-realtime-preview.el
+++ org-realtime-preview.el
@@ -2,10 +2,10 @@
 (require 'eww)
 
 (defvar org-preview-output-file-name "realtime-preview-result.html"
-  "预览产生的临时文件名称")
+  "temporary file generated when preview")
 
 (defun org-preview--select-or-create-buffer-window (buffer-or-name)
-  "若frame中有显示`buffer-or-name'的window,则选中该window,否则创建新window显示该buffer"
+  "If any window in currect frame displaying BUFFER-OR-NAME ,then select the window, otherwise,create a new window to display it"
   (let ((buf (get-buffer-create buffer-or-name)))
 	(unless (get-buffer-window buf)
 	  (split-window)
@@ -13,15 +13,16 @@
 	(select-window (get-buffer-window buf))))
 
 (defun org-preview--buffer-point(buffer-or-name &optional default-point)
-  "获取指定buffer的光标所在位置
+  "Get the point position in specify buffer
 
-若参数`buffer-or-name'没有对应buffer,则返回`default-point'"
+If BUFFER-OR-NAME did not exist, return DEFAULT-POINT"
   (if (get-buffer buffer-or-name)
 	  (with-current-buffer buffer-or-name
 		(point))
 	default-point))
+
 (defun org-preview-convert (output-file-name)
-  "导出org为`output-file-name'中,并调用`eww-open-file'来查看"
+  "Export current org-mode buffer to OUTPUT-FILE-NAME, and call `eww-open-file' to preview it"
   (let ((cb (current-buffer))
 		(eww-point (org-preview--buffer-point "*eww*" 1)))
     (save-excursion
@@ -32,17 +33,17 @@
     (org-preview--select-or-create-buffer-window cb)))
 
 (defun org-preview ()
-  "导出org为`org-preview-output-file-name'中,并调用`eww-open-file'来查看"
+  "Export current org-mode buffer to `org-preview-output-file-name', and call `eww-open-file' to preview it"
   (interactive)
   (org-preview-convert org-preview-output-file-name))
 
 (defun org-preview-turn-on-preview-at-save ()
-  "开启保存后自动预览"
+  "turn on automatically preview current org-file when save"
   (interactive)
   (add-hook 'after-save-hook #'org-preview nil t))
 
 (defun org-preview-turn-off-preview-at-save ()
-  "关闭保存后自动预览"
+  "turn off automatically preview current org-file when save"
   (interactive)
   (remove-hook 'after-save-hook #'org-preview t))
 
