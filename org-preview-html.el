d2212dd102e8d50a3cfc720341d8249008854ee7
jakebox <jacobboxerman@gmail.com>
2021-09-07 15:00:40 -0500

    Updated README/commentary

--- org-preview-html.el
+++ org-preview-html.el
@@ -25,22 +25,19 @@
 ;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-;;
 ;; This minor mode provides a side-by-side preview of your org-exported HTML
 ;; files using the either the eww or xwidget browsers. The update frequency of
 ;; the preview can be configured to suit your preference.
 ;;
-
-;;; Source code
-;;
-;; org-preview-html's code can be found here:
-;;   http://github.com/jakebox/org-preview-html
-
-;;; Quick start:
+;; Quick start:
 ;; Put this file under your load path.
 ;; Enable the minor mode in an Org buffer:
 ;;   M-x org-preview-html-mode
 ;; Configure options with M-x customize-group org-preview-html
+;;
+;; Source code
+;; org-preview-html's code can be found here:
+;;   http://github.com/jakebox/org-preview-html
 
 ;;; Code:
 
